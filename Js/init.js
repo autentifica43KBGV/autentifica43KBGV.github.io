72e9ebb91c169fd0c4e91e6094d4ad4319fee29e
autentifica43KBGV <81201123+autentifica43KBGV@users.noreply.github.com>
2021-03-22 19:01:49 -0600

    Update init.js

--- Js/init.js
+++ Js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyA0eItZg_xZzAhjML3w7Li1zPXxbCUHqk8",
-    authDomain: "autentifica43.firebaseapp.com",
-    projectId: "autentifica43",
-    storageBucket: "autentifica43.appspot.com",
-    messagingSenderId: "587803819098",
-    appId: "1:587803819098:web:adb2faec95942b64392c63",
-    measurementId: "G-CMTB5FT43S"
+   apiKey: "AIzaSyBmzBsmvS1z1t9akbzH-zS03L2cpp7C0G8",
+    authDomain: "autentifica43kbgv-7f777.firebaseapp.com",
+    projectId: "autentifica43kbgv-7f777",
+    storageBucket: "autentifica43kbgv-7f777.appspot.com",
+    messagingSenderId: "301249303930",
+    appId: "1:301249303930:web:a4fd16a99cb90dd7a6b2c3",
+    measurementId: "G-WK479LN3M4"
     });
